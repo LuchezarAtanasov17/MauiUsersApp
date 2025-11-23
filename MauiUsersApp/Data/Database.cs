0cdf067c9623da50f7899af4f51d6bcd87e1ea5e
Лъчезар Атанасов <lachezaratanasov05@pmgkk.com>
2025-11-24 00:43:33 +0200

    Seed data in the database.

--- MauiUsersApp/Data/Database.cs
+++ MauiUsersApp/Data/Database.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.Data.Sqlite;
+﻿using Dapper;
+using Microsoft.Data.Sqlite;
 
 namespace MauiUsersApp.Data;
 
@@ -8,12 +9,12 @@ public static class Database
 
     public static void Initialize()
     {
+        using var connection = GetConnection();
+
+        connection.Open();
+
         if (!File.Exists(dbPath))
         {
-            using var connection = GetConnection();
-
-            connection.Open();
-
             SqliteCommand tableCommand = connection.CreateCommand();
 
             tableCommand.CommandText =
@@ -27,6 +28,16 @@ public static class Database
                         IsActive INTEGER NOT NULL)";
 
             tableCommand.ExecuteNonQuery();
+
+        }
+        var count = connection.ExecuteScalar<int>("SELECT COUNT(*) FROM Users");
+
+        if (count == 0)
+        {
+            connection.Execute(@"INSERT INTO Users (Name, Username, Email, Phone, Password, IsActive)
+                                    VALUES ('Test User 1', 'testuser1', 'test1@example.com', '1111111111', '123', 1),
+                                            ('Test User 2', 'testuser2', 'test2@example.com', '2222222222', '123', 1),
+                                            ('Test User 3', 'testuser3', 'test3@example.com', '3333333333', '123', 0)");
         }
     }
 
